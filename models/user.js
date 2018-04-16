9a078cc9675db54f4a3560c2fec7d0298dd042a1
Devendra Shah <Devendra48D@users.noreply.github.com>
2018-04-16 17:54:18 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -29,13 +29,13 @@ var userSchema = mongoose.Schema({
 
 });
 
-// methods ======================
+// methods ====================
 // generating a hash
 userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-// checking if password is valid
+// checking if the password is valid
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
